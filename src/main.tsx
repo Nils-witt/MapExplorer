2acb83e5ec452404338bc0d0325cf5c931f80ab4
Nils Witt <nils.witt@nils-witt.de>
2026-08-05 13:37:16 +0200

    feat: wrap App component with ThemeProvider and CssBaseline

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 import './style.scss';
 import { App } from './App.tsx';
 
+const theme = createTheme();
+
 createRoot(document.getElementById('app')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
